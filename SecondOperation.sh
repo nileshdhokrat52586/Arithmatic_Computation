afaf5eacb3612595b04762e188738faba352cd99
unknown <nileshdhokrat52586@gmail.com>
2020-08-28 20:14:50 +0530

    Added Second task

---
+++ SecondOperation.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -x
+
+function input()
+{
+read -p "Enter the a input" input1
+read -p "Enter the b input" input2
+read -p "Enter the c input" input3
+
+}
+
+function firstEquation()
+{
+        input
+        firstEquationResult=$(( $input1 + $input2 + $input3))
+}
+firstEquation
+function secondEquation()
+{
+	
+	input
+	secondEquationResult=$(( $numberOne * $numberTwo + $numberThree ))
+}
+secondEquation
