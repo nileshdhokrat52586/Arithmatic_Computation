8c6bd609d1b1b4960cbaa9e1661c4932ae7d1a54
unknown <nileshdhokrat52586@gmail.com>
2020-08-28 19:57:17 +0530

    Added:first of Arithmatic Calculation

---
+++ ArithmaticOperation.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -x
+
+function input()
+{
+read -p "Enter the a input" input1
+read -p "Enter the b input" input2
+read -p "Enter the c input" input3
+
+}
+
+function firstEquation()
+{
+	input
+	firstEquationResult=$(( $input1 + $input2 + $input3))
+}
+firstEquation
